cc2ce6a1231a125744a714288d4ced80e84deca5
eric <eric.ostlund@proton.me>
2024-05-08 16:32:04 +0200

    potions are somewhat ready

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -2,27 +2,47 @@ package items;
 
 public class Potion {
 
-  String name;
+  String useText;
+  String throwText;
   String description;
+  String type;
   int healthRestored;
   int damage;
 
   public Potion() {
   }
 
-  public Potion(String name, String description, int healthRestored, int damage) {
-    this.name = name;
+  public Potion(String throwText, String type, String useText, String description, int healthRestored, int damage) {
+    this.useText = useText;
     this.description = description;
     this.healthRestored = healthRestored;
     this.damage = damage;
+    this.type = type;
+    this.throwText = throwText;
   }
 
-  public String getName() {
-    return name;
+  public String getThrowText() {
+    return throwText;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setThrowText(String throwText) {
+    this.throwText = throwText;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getUseText() {
+    return useText;
+  }
+
+  public void setUseText(String damageText) {
+    this.useText = damageText;
   }
 
   public String getDescription() {
@@ -51,16 +71,40 @@ public class Potion {
 
   public Potion getHealthPotion(int healthRestored) {
     Potion potion = new Potion();
+    potion.setType("health potion");
     potion.setHealthRestored(healthRestored);
-    potion.setDescription("Just your normal, green potion. Heals " + healthRestored + " healthpoints when drunk.");
+    potion.setDescription("Just your normal, green potion.");
+    potion.setThrowText("For some reason you throw the health potion.");
     return potion;
   }
 
   public Potion getPoisonPotion(int healthRestored, int damage) {
     Potion potion = new Potion();
+    potion.setType("bottle of poison");
     potion.setHealthRestored(healthRestored);
     potion.setDamage(damage);
-    setDescription("This looks fishy. Really fishy.");
+    potion.setDescription("This looks fishy. Really fishy.");
+    potion.setThrowText("You throw the bottle. The green liquid gets all over HMMMM hur ska jag få in fienden här?");
+    return potion;
+  }
+
+  public Potion getFirePotion(int damage) {
+    Potion potion = new Potion();
+    potion.setDamage(damage);
+    potion.setType("bottle of liquid fire");
+    potion.setDescription("This looks... Spicy?");
+    potion.setUseText("You take a zip and catch on fire. You take " + damage + " damage. Dingus.");
+    potion.setThrowText("You throw the potion, and BLABLABLA catches fire!!");
+    return potion;
+  }
+
+  public Potion getManaPotion(int mana) {
+    Potion potion = new Potion();
+    potion.setHealthRestored(mana);
+    potion.setType("mana potion");
+    potion.setDescription("A beautiful blue potion. How about you try it?");
+    potion.setUseText("You drink it. It feels like a cold stream down your throat.");
+    potion.setThrowText("You throw it. Nothing happends.");
     return potion;
   }
 }
